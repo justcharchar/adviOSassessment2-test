8a40d9a817a7c0fcec62ef9ca1f80b7a15d7ff86
justcharchar <charlizetahimic@gmail.com>
2025-08-23 18:19:53 +1000

    Initial Commit

---
+++ adviOSassessment2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  adviOSassessment2
+//
+//  Created by Ceri Tahimic on 23/8/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ adviOSassessment2/adviOSassessment2App.swift
@@ -0,0 +1,17 @@
+//
+//  adviOSassessment2App.swift
+//  adviOSassessment2
+//
+//  Created by Ceri Tahimic on 23/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct adviOSassessment2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
